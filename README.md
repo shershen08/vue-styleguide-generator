77ae27e80c74a1c19f4548f5ff0851d5fc69c477
Mikhail Kuznetcov <michail.kuznetsov@gmail.com>
2017-01-22 23:32:52 +0100

    added readme and more details to output

---
+++ README.md
@@ -0,0 +1,12 @@
+## Vue js compoentns style guide generator
+
+Initially started after reading [this feature request](https://github.com/vuejs/vue-requests/issues/17).
+Now only basic proof of concept is availbale which can load .vue file, parse it and extract basic props of it.
+Target is to have some tool with at least som of the [React version](https://github.com/styleguidist/react-styleguidist) capabilities.
+
+### How to try
+
+ - Clone ot download this repo
+ - ```npm install```
+ - ```node index.js``` 
+ - you'll see simple console output of the 'VueSound.vue' file
--- index.js
+++ index.js
@@ -1,3 +1,40 @@
 var path = require('path')
+var parse = require('./lib/parser')
+var fs = require('fs')
+var babel = require('babel-core')
+const vm = require('vm')
+const util = require('util')
+// var args = process.argv.slice(2)
 
-let loadFile = './example-components/VueSound.vue'
+// 1 load
+// /replace with comand line or some more inteligent way
+let filename = 'VueSound.vue'
+let loadFile = './example-components/' + filename
+let vueFile = fs.readFileSync(loadFile, {encoding: 'utf-8'})
+
+// 2 parse
+var parts = parse(vueFile, 'name', false)
+var componentScript = parts.script.content
+
+// 3 babel
+const options = {
+  ast: false,
+  comments: false,
+  presets: ['es2015']
+}
+var result = babel.transform(componentScript, options)
+
+// 4 safe eval
+const sandbox = {
+  exports: {}
+}
+const script = new vm.Script(result.code, {})
+const context = new vm.createContext(sandbox)
+script.runInContext(context)
+const runResults = util.inspect(sandbox)
+const component = sandbox.exports.default
+
+console.log('\n\nComponent', filename)
+console.log('conponent name:', component.name)
+console.log('conponent props:', Object.keys(component.props))
+console.log('conponent methods:', Object.keys(component.methods))
